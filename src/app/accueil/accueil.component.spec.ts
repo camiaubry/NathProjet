27c04158374cb94e2129d312e1d4c3119feabe4e
celine.degea <celine.de-gea_ext@michelin.com>
2019-01-03 23:18:38 +0100

    page_menu_03012019

---
+++ src/app/accueil/accueil.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccueilComponent } from './accueil.component';
+import {expect, describe} from "@angular/core/testing/src/testing_internal";
+
+describe('AccueilComponent', () => {
+  let component: AccueilComponent;
+  let fixture: ComponentFixture<AccueilComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AccueilComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AccueilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {AccueilComponent} from "./accueil/accueil.component";
+
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent }
+
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
